12fe210c561fcda1485c5b63d33a9e437aea9e46
Rogerio Nascimento <roger.asnascimento@gmail.com>
2021-03-09 22:17:54 -0300

    connection mongoDB db.js and handled error

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(chalk.bgGrey(`MongoDB Connected: ${conn.connection.host}`));
+  } catch (error) {
+    console.log(chalk.bgRed('Connection Error: ', error));
+    conn.close();
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const chalk = require('chalk');
-
-// middleware
-const logger = require('./middleware/logger');
-
-// router files
-const bootcamp = require('./routes/bootcamp');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 const API_URL = process.env.API_URL || '/api/v1';
 
+// Connect to database
+connectDB();
+
+// middleware
+// const logger = require('./middleware/logger');
+
+// router files
+const bootcamp = require('./routes/bootcamp');
+
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  // app.use(logger);
+}
 
 // Mount routers
 app.use(`${API_URL}/bootcamps`, bootcamp);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    chalk.yellow.inverse(
+    chalk.blue.inverse(
       `Server running in ${process.env.NODE_ENV} on port ${PORT}`
     )
   );
+});
+
+// Handle unhlandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(chalk.bgMagenta(`Error: ${err.message}`));
+
+  // close server & exite processe
+  server.close(() => process.exit(1));
 });
