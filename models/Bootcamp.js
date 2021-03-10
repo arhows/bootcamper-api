c87e1be1015cfdacbab90368c72179f1f533b0c3
Rogerio Nascimento <roger.asnascimento@gmail.com>
2021-03-09 22:48:32 -0300

    data json and models

---
+++ models/Bootcamp.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlenght: [50, 'Name can not be more than 50 characters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    trim: true,
+    maxlenght: [500, 'Description can not be more than 500 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /http?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlenght: [20, 'Phone number can not be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an adress'],
+  },
+  location: {
+    //   GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+});
